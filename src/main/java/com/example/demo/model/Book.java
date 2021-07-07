d94e689cae80fc7b23239f497958e8b8fbe11eb2
MACBOOK AIR <macbookair@MACBOOKs-MacBook-Air.local>
2021-07-07 01:42:24 -0400

    ehcache integration in springboot.

--- src/main/java/com/example/demo/model/Book.java
+++ src/main/java/com/example/demo/model/Book.java
@@ -1,12 +1,14 @@
 package com.example.demo.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Book {
+public class Book implements Serializable{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
